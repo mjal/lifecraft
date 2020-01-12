0cecf21bc2537f829093b37e15d214c9fabccdf0
Maxime Lalisse <maxime.lalisse@gmail.com>
2020-01-12 21:47:01 +0100

    update tmux config

--- tmux.sh
+++ tmux.sh
@@ -6,9 +6,7 @@ tmux new-session -d -s lifecraft
 
 tmux selectp -t 1 
 tmux send-keys "vim src/main.ml +NERDTreeToggle" C-m 
-
-# Split pane 1 horizontal by 65%, start redis-server
-tmux splitw -v -p 30
+tmux splitw -v -p 10
 tmux send-keys "bsb -w" C-m
 tmux splitw -h -p 20
 tmux send-keys "yarn watch-bundle" C-m
