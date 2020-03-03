0007d23fccb6adc73935fb330ed0f15ad848185c
Maxime Lalisse <maxime.lalisse@gmail.com>
2020-03-03 10:55:11 +0100

    update tmux script

--- tmux.sh
+++ tmux.sh
@@ -5,7 +5,7 @@ tmux start-server
 tmux new-session -d -s lifecraft
 
 tmux selectp -t 1 
-tmux send-keys "vim src/main.ml +NERDTreeToggle" C-m 
+#tmux send-keys "vim src/main.ml +NERDTreeToggle" C-m 
 tmux splitw -v -p 10
 tmux send-keys "bsb -w" C-m
 tmux splitw -h -p 20
